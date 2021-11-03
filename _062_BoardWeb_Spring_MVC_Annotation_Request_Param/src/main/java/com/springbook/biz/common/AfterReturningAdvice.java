375bb75aba27c2a25d6107ecefd1a0434b188fd7
leeyou31 <81919273+leeyou31@users.noreply.github.com>
2021-11-03 18:09:28 +0900

    Add files via upload

---
+++ _062_BoardWeb_Spring_MVC_Annotation_Request_Param/src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
@@ -0,0 +1,29 @@
+package com.springbook.biz.common;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
+
+import com.springbook.biz.user.UserVO;
+
+@Service
+@Aspect
+public class AfterReturningAdvice {
+	//returnObj : 포인트컷 메소드 실행 후 리턴 값을 받을 객체
+	//@Before("getPointcut()")
+	//JoinPoint 객체를 제외한 매개변수가 존재할때는 속성값을 따로 지정해줘야됨
+	@AfterReturning(pointcut="PointcutCommon.getPointcut()", returning="returnObj")
+	public void afterReturningAdvice(JoinPoint jp, Object returnObj) {
+		String method = jp.getSignature().getName();
+		if(returnObj instanceof UserVO) {
+			UserVO user = (UserVO)returnObj;
+			if(user.getRole().equals("Admin")) {
+				System.out.println(user.getName() + " 로그인(Admin)");
+			}
+		}
+		
+		System.out.println("[사후 처리] " + method + "() 메소드 리턴 값 : " + returnObj.toString());
+	}
+}
